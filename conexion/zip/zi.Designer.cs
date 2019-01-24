a6fb011697906c9536857090dbcd90f195ba6d28
Marc <43479593+MORATA22@users.noreply.github.com>
2019-01-24 16:13:33 +0100

    zip

--- conexion/zip/zi.Designer.cs
+++ conexion/zip/zi.Designer.cs
@@ -28,20 +28,61 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.butcomp = new System.Windows.Forms.Button();
+            this.butdesc = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.butdesc);
+            this.groupBox1.Controls.Add(this.butcomp);
+            this.groupBox1.Location = new System.Drawing.Point(280, 113);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(207, 137);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "ZIPIZAPE";
+            // 
+            // butcomp
+            // 
+            this.butcomp.Location = new System.Drawing.Point(69, 39);
+            this.butcomp.Name = "butcomp";
+            this.butcomp.Size = new System.Drawing.Size(75, 23);
+            this.butcomp.TabIndex = 0;
+            this.butcomp.Text = "ZIP";
+            this.butcomp.UseVisualStyleBackColor = true;
+            this.butcomp.Click += new System.EventHandler(this.butcomp_Click);
+            // 
+            // butdesc
+            // 
+            this.butdesc.Location = new System.Drawing.Point(69, 80);
+            this.butdesc.Name = "butdesc";
+            this.butdesc.Size = new System.Drawing.Size(75, 23);
+            this.butdesc.TabIndex = 1;
+            this.butdesc.Text = "UPZIP";
+            this.butdesc.UseVisualStyleBackColor = true;
+            this.butdesc.Click += new System.EventHandler(this.butdesc_Click);
             // 
             // zi
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
             this.Name = "zi";
             this.Text = "ZIP";
+            this.groupBox1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button butdesc;
+        private System.Windows.Forms.Button butcomp;
     }
 }
 
--- conexion/zip/zi.cs
+++ conexion/zip/zi.cs
@@ -7,14 +7,80 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.IO.Compression;
 
 namespace zip
 {
     public partial class zi : Form
     {
+        //Vatiables para comprimir y descomprimir
+        const string Compri = @"archivo\01hola";
+        const string CompressArchive = @"archivo\pruebazip.zip";
+        const string Decompri = @"archivo";        
+
         public zi()
         {
             InitializeComponent();
+        }
+        
+        private void Comprimir()
+        {
+            try
+            {
+                //ZipFile utilizado para trabjar con Zips
+                //CreateFromDirectory creamos el archivo zip, la primera variable, compri, es nuestro archivo y la segunda, CompressArchive, el resultado(CompressArchive la variable que guarda  el zip creado)
+                ZipFile.CreateFromDirectory(Compri, CompressArchive);
+            }
+            catch (IOException)
+            {
+                //Borra el archivo indicado
+                File.Delete(CompressArchive);
+                //Creamos completamente nuevo el zip
+                ZipFile.CreateFromDirectory(Compri, CompressArchive);
+            }
+            catch (UnauthorizedAccessException)
+            {
+                //Mensaje de error si hay algún problema
+                MessageBox.Show("Error al comprimir");
+            }
+        }
+        
+        private void Descomprimir()
+        {
+            try
+            {
+                //ExtractToDirectory estraemos los arhivos del zip, CompressArchive nuestro archivo(zip) Decompri resultado de la descomprsión
+                ZipFile.ExtractToDirectory(CompressArchive, Decompri);
+            }
+            catch (IOException)
+            {
+                ////Directory nos permite trabajar con archivos entre directorios y subdirectorios
+                ///Directory.GetFiles Devuelve los nombres de archivos
+                ///Directory.GetDirectories Devuelve los nombres de subdirectorios
+                ///Creamos dos arrays y después los recorremos con un foreach
+
+                string[] FilePath = Directory.GetFiles(Decompri);
+                string[] carpetas = Directory.GetDirectories(Decompri);
+                foreach (string fp in FilePath) File.Delete(fp);
+                foreach (string cp in carpetas) Directory.Delete(cp);
+                // ExtractToDirectory Extraemos todos los archivos de un zip
+                ZipFile.ExtractToDirectory(CompressArchive, Decompri);
+            }
+            catch (UnauthorizedAccessException)
+            {
+                MessageBox.Show("Error al descomprimir");
+            }
+        }
+
+        private void butcomp_Click(object sender, EventArgs e)
+        {
+            Comprimir();
+        }
+
+        private void butdesc_Click(object sender, EventArgs e)
+        {
+            Descomprimir();
         }
     }
 }
