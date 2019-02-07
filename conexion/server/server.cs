53520d0256bc9193735d4d1d0cf96e46b7e4cdc1
Marc <43479593+MORATA22@users.noreply.github.com>
2019-02-07 18:26:55 +0100

    kk

--- conexion/server/server.cs
+++ conexion/server/server.cs
@@ -15,7 +15,9 @@ using System.IO;
 namespace server
 {
     public partial class server : Form
-    {       
+    {
+        TcpListener lisen = null;
+        public TcpClient cliente = null;
         public server()
         {
             InitializeComponent();
@@ -24,20 +26,18 @@ namespace server
         private void server_Load(object sender, EventArgs e)
         {
            labestado.Text = "Encendido";
-            //instanciamos un thread que ejecute la función conex
-          
-            Thread serverThread = new Thread(conex);           
+            //instanciamos un thread que ejecute la función conex          
+            Thread serverThread = new Thread(conex);            
             // lo inicimaos con start
             serverThread.SetApartmentState(ApartmentState.STA);           
-            serverThread.Start();
+            serverThread.Start();           
 
         }
 
         private void conex()
-        {
-            //instancias un cliente y un server tcp para verse
-            TcpListener lisen = null;
-            TcpClient cliente = null;
+        {            //instancias un cliente y un server tcp para verse
+            
+           
             int i = 0;
 
             while (true)
@@ -81,6 +81,7 @@ namespace server
                             //Le ponemos al bucle que con Streamread mire si hay algo en el array de bytes mientras sea diferente ha 0 entrará en el bucle
                             while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
                             {
+                                
                                 //cadena recibida
                                 mensaje = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
 
@@ -133,29 +134,13 @@ namespace server
         }
        
         public void ReceiveFiles()
-        {
-            int port = 1000;
-            IPAddress IPA = IPAddress.Parse("127.0.0.1");
-            TcpListener _Listener = new TcpListener(IPA, port);
-            NetworkStream _nStream;
-            TcpClient _Client = new TcpClient();
-            const int _BufferSize = 1024;
-            try
-            {              
-                ///Instanciamos un nuevo TcpListener, que escuche a cualquier ip
-                ///y por el puerto que usamos
-               
-                ///Iniciamos el listener
-                _Listener.Start();
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
+        {           
+         
+            const int _BufferSize = 1024;        
 
             byte[] RecData = new byte[_BufferSize];
             int RecBytes;
-
+            NetworkStream stream;
             for (; ; )
             {
                 string _Status = string.Empty;
@@ -168,13 +153,13 @@ namespace server
                     DialogResult result;
                     ///si hay solicitudes de conexion pendientes hace todo esto,
                     ///sino simplemente sigue escuchando
-                    if (_Listener.Pending())
+                    if (lisen.Pending())
                     {
                         ///Indicamos al Listener que acepte las solicitudes de
                         ///conexion pendiente de el TCPClient
-                        _Client = _Listener.AcceptTcpClient();
+                        cliente = lisen.AcceptTcpClient();
                         // Get a stream object for reading and writing
-                        _nStream = _Client.GetStream();
+                        stream = cliente.GetStream();
                         _Status = "Connected to a client\n";
                         result = MessageBox.Show(message, caption, buttons);
                         ///Si la respuesta del messagebox ha sido que si que queremos recibir el fichero
@@ -203,7 +188,7 @@ namespace server
                                 ///que lo abra o lo cree si no existe, le damos permisos de escritura
                                 FileStream Fs = new FileStream(SaveFileName, FileMode.OpenOrCreate, FileAccess.Write);
                                 ///Mientras que el buffer sea mas grande a 0 sigue con el bucle
-                                while ((RecBytes = _nStream.Read(RecData, 0, RecData.Length)) > 0)
+                                while ((RecBytes = stream.Read(RecData, 0, RecData.Length)) > 0)
                                 {
                                     ///Escribe lo que hay en el buffer
                                     Fs.Write(RecData, 0, RecBytes);
@@ -211,8 +196,8 @@ namespace server
                                 }
                                 ///Cerramos todo
                                 Fs.Close();
-                                _nStream.Close();
-                                _Client.Close();
+                                stream.Close();
+                                cliente.Close();
                             }                   
                         }
                     }
